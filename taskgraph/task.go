fbf52606feef74964190e3dffcb6529f566902a9
Yalhash <yalhash@DESKTOP-AEGL8LS.localdomain>
2019-10-16 15:15:05 -0400

    redoing task graph data structure

---
+++ taskgraph/task.go
@@ -0,0 +1,21 @@
+package taskgraph
+
+//Status of Task
+type Status int 
+
+//status enum
+const (
+	_ = iota
+	NotReady
+	ReadyWorking
+	Finished
+	End
+)
+
+//Task is the object that needs to be scheduled
+type Task struct {
+	ID string
+	Time uint
+	Requirements []*Task
+	WorkStatus Status
+}
---
+++ taskgraph/taskGraph.go
@@ -0,0 +1,11 @@
+package taskgraph
+
+
+//TaskGraph maps task ID's to tasks
+type TaskGraph map[string]Task
+
+//Task of ID "" is the end task and will have a Status of End
+
+
+
+
